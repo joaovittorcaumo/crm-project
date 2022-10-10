e9b071e0e11dd9fa2a508abb1d189bf8641e8a78
joao.caumo <joao.caumo@edu.pucrs.br>
2022-10-10 13:52:07 -0300

    🚧 Structuring user main page

---
+++ front-end/src/pages/UserMainPage/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserMainPage: React.FC = () => {
+    return (
+        <div className="container">
+            <div className="content">
+                <div className="textContainer"><h1 className="title"></h1>
+                <p className="subtitle"></p></div>
+                <div className="worfFlowAnalysis"></div>
+                <div className="projects"></div>
+            </div>
+        </div>
+    )
+
+}
+
+export default UserMainPage;
