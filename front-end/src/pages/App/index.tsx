6b8ba552107b60d716f428cdfde569eb61fbdc23
joao.caumo <joao.caumo@edu.pucrs.br>
2022-10-10 10:48:02 -0300

    ✅ Finishing landing  page structure

--- front-end/src/pages/App/index.tsx
+++ front-end/src/pages/App/index.tsx
@@ -1,56 +1,61 @@
-import React from 'react';
+import React from "react";
 
-import './index.scss';
+import "./index.scss";
+import imgSplashContainer from "../../assets/LandingPage/imgSplashContainer.png";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="container">
       <div className="content">
-      <section className="splashContainer">
-        <div className="left">
-          <div className="text">
-            <h1 className="title">Por que a Jv CRMS:</h1>
-            <p className="paragraph">
-              Fomos criados no intuito de facilitar a vida das empresas, seus
-              funcionarios e clientes!
-            </p>
-          </div>
-          <button className="createAccountBtn">Cadastre seu perfil!</button>
-        </div>
-        <div className="Right"></div>
-      </section>
-
-      <section className="aboutSection">
-        <h1 className="title">O que fazemos?</h1>
-        <div className="aboutSectionDiv">
-          <div className="top">
-            <div className="content">
-              <div className="aboutSectionDiv">
-                <h4 className="title">
-                  Conectamos a sua empresa a novos clientes
-                </h4>
-              </div>
+        <section className="splashContainer">
+          <div className="left">
+            <div className="text">
+              <h1 className="title">Por que a Jv CRMS:</h1>
               <p className="paragraph">
-                Nossa plataforma tem eficiencia em conectar sa empresa a varios
-                clientes por meio de um sistema inteligente de mapeamento
+                Fomos criados no intuito de facilitar a vida das empresas, seus
+                funcionarios e clientes!
               </p>
             </div>
+            <button className="createAccountBtn">Cadastre seu perfil!</button>
+          </div>
+          <div className="Right">
+            <img className="image" src={imgSplashContainer} alt="Uma imagem de conexoes de rede" />
+          </div>
+        </section>
 
-            <div className="content">
-              <div className="aboutSectionDiv">
-                <h4 className="title">Inovando o sistema de CRMS's</h4>
+        <section className="aboutSection">
+          <h1 className="title">O que fazemos?</h1>
+          <div className="aboutSectionDiv">
+            <div className="top">
+              <div className="textContent">
+                <div className="aboutSectionDiv">
+                  <h4 className="title">
+                    Conectamos a sua empresa a novos clientes
+                  </h4>
+                </div>
+                <p className="paragraph">
+                  Nossa plataforma tem eficiencia em conectar sa empresa a
+                  varios clientes por meio de um sistema inteligente de
+                  mapeamento
+                </p>
               </div>
-              <p className="paragraph">
-                Com a JV CRM's, voce consegue controlar que times irao trabalhar
-                no seu projeto, e ate mesmo definir seus colaboradores
-              </p>
+
+              <div className="textContent">
+                <div className="aboutSectionDiv">
+                  <h4 className="title">Inovando o sistema de CRMS's</h4>
+                </div>
+                <p className="paragraph">
+                  Com a JV CRM's, voce consegue controlar que times irao
+                  trabalhar no seu projeto, e ate mesmo definir seus
+                  colaboradores
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
       </div>
     </div>
   );
-}
+};
 
 export default App;
